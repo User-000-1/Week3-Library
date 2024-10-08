f6e627162d805a9b2698b0ec3d1196ecfa7f3f9e
Harvey Laird <Laird-H2@ulster.ac.uk>
2024-10-08 10:04:27 +0100

    second commit

--- Week3_Library/Program.cs
+++ Week3_Library/Program.cs
@@ -1,6 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
 
-Console.WriteLine("test test test");
+    void DisplayInfo()
+    {
+        Console.WriteLine("Book information");
+        Console.WriteLine("________________");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+    }
+        static void main(string[] args)
+    {
+        Book book = new Book();
+        Book.Title = "C# for beginners";
+        Book.Author = "Bill Gates";
+        Book.ISBN = "1234567";
+
+        Book Book2 = new Book();
+        Book.Title = "";
+        Book.Author = "";
+        Book.ISBN = "";
 
 
+    }
+}
