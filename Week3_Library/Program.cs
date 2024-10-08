1e199e8af5bb6e6de8394a449cfcb00ba4a5d6f1
Harvey Laird <Laird-H2@ulster.ac.uk>
2024-10-08 11:04:08 +0100

    6th commit

--- Week3_Library/Program.cs
+++ Week3_Library/Program.cs
@@ -1,14 +1,28 @@
 ﻿
 using System;
+using System.Net.Http.Headers;
 class Book
 {
+    //Properties
     string Title;
     string Author;
     string ISBN;
     int NoOfPages;
+    private string bookISBN;
+
+    //Constructor for Book object
+    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages )
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+        NoOfPages = bookNoOfPages;
+
+    }
 
     void DisplayInfo()
     {
+        //Output
         Console.WriteLine("Book information");
         Console.WriteLine("________________");
         Console.WriteLine($"Book title: {Title}");
@@ -18,20 +32,12 @@ class Book
     }
         static void Main(string[] args)
     {
-        Book book = new Book();
-        book.Title = "C# for beginners";
-        book.Author = "Bill Gates";
-        book.ISBN = "1234567";
-        book.NoOfPages = 200;
-
-        Book book2 = new Book();
-        book2.Title = "C# Second book";
-        book2.Author = "Microsoft";
-        book2.ISBN = "12345678";
-        book2.NoOfPages = 50;
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567", 200);
+        Book book2 = new Book("C# for beginners", "Bill Gates", "1234567", 200);
 
         book.DisplayInfo();
         book2.DisplayInfo();
+
 
 
     }
